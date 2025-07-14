c3a7250e6948d7b37312ae1301b910b5b865592f
ɓ૨σҡεɳ3 <124243079+spivan19@users.noreply.github.com>
2025-07-14 11:02:01 +0100

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -18,8 +18,8 @@ async function aliveCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '',
+                    newsletterName: 'ƙαɱι-ɱԃ',
                     serverMessageId: -1
                 }
             }
